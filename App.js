45d561b9478d0a026a7c5d0e4262d740075953b1
yishoyoung <syoung98@naver.com>
2025-04-07 18:50:32 +0900

    숫자 증감 기능 구현

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import React, {useState} from "react";
 
 export default function App() {
+  const [number, setNumber] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontSize:50}}>결과 : {number}</Text>
+      <View style={{flexDirection:"row" , gap:10}}>
+        <Button title="증가" onPress={()=>setNumber(number+1)}/>
+        <Button title="감소" onPress={()=>setNumber(number-1)}/>      
+      </View>
       <StatusBar style="auto" />
     </View>
   );
